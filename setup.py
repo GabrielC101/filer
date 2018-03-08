f577d2d6266f042aa1c5ff660735414750a8f892
Gabriel Curio <g.curio@gmail.com>
2018-03-08 10:36:45 -0600

    .25 version submitted to pypi.

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='filer',
-    version='0.24',
+    name='filery',
+    version='0.25',
+    description='File Path API for Python.',
     packages=[
         'filer',
         'filer.ffmpeg_wrapper',
         'filer.ffmpeg_wrapper.parsers',
               ]
 ,
-    url='',
-    license='',
+    url='https://github.com/GabrielC101/filer',
     author='Gabriel Curio',
     author_email='',
-    description='',
     install_requires=[
         'marshmallow',
         'python-magic',
         'six',
         'xxhash'
-    ]
+    ],
+    keywords=['system', 'linux'],
 )
